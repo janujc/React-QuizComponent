abf020d3d769f4a5565de681b926f206af2c44ec
Januario Carreiro <jjc70@duke.edu>
2020-06-26 20:40:39 -0400

    Completed module 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
-import './App.css'
-import Quiz from './Quiz.js'
+import React, { Component } from 'react';
+import './App.css';
+import Quiz from './Quiz.js';
 
 class App extends Component {
   render() {
     return (
       <Quiz />
-    )
+    );
   }
 }
 
-export default App
+export default App;
